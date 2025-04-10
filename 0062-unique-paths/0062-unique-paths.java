9a0b483d041093e73e8cbf0919d9449f13cc3bc3
Drishti Aggarwal <drishtiaggarwal6284@gmail.com>
2025-04-10 17:10:00 +0530

    Time: 0 ms (100%), Space: 40.8 MB (26.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,24 @@
 class Solution {
-     static int countWaysUtil(int i, int j, int[][] dp) {
-    
-        if (i == 0 && j == 0)
-            return 1;
-        
-     
-        if (i < 0 || j < 0)
-            return 0;
 
-   
-        if (dp[i][j] != -1)
-            return dp[i][j];
+
+    public static int unique(int i, int j,int[][]dp) {
+        if(i<0 || j<0 ) return 0;
+        if(i == 0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int up = unique(i-1,j,dp);
+        int left = unique(i,j-1,dp);
 
      
-        int up = countWaysUtil(i - 1, j, dp);
-        int left = countWaysUtil(i, j - 1, dp);
-
-   
         return dp[i][j] = up + left;
     }
 
     public int uniquePaths(int m, int n) {
-         int dp[][] = new int[m][n];
-        
- 
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
+        int [][] dp = new int[m][n];
 
-        // Start the recursive calculation from the bottom-right cell (m-1, n-1)
-        return countWaysUtil(m-1 , n-1 , dp);
+        for (int i =0;i<m;i++) {
+            Arrays.fill(dp[i],-1);
+        }
+        return unique(m-1,n-1,dp);
     }
 }

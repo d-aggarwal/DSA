c1353a71a83d80e3d6df21df69b7edbaec80e0d8
Drishti Aggarwal <drishtiaggarwal6284@gmail.com>
2026-01-27 17:18:35 +0530

    Time: 16 ms (0.63%), Space: 45.1 MB (8.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,19 +9,31 @@
  * }
  */
 class Solution {
+
+    public static ListNode iterative(ListNode head) {
+        if(head.next ==null) return head;
+
+        ListNode r = iterative(head.next);
+        System.out.println(head.val);
+        head.next.next = head;
+        head.next = null;
+        return r;   
+    }
     public ListNode reverseList(ListNode head) {
         if(head==null || head.next==null) return head;
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = head.next ;
+        // ListNode prev = null;
+        // ListNode curr = head;
+        // ListNode next = head.next ;
 
-        while(curr !=null) {
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-            if(curr!=null)next = curr.next;
-        }
+        // while(curr !=null) {
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = next;
+        //     if(curr!=null)next = curr.next;
+        // }
 
-        return prev;
+        // return prev;
+
+        return iterative(head);
     }
 }

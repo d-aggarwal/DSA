f2343b3f7c14b5427077b36b8cdfc16ea7131fe0
Drishti Aggarwal <drishtiaggarwal6284@gmail.com>
2026-02-19 17:57:17 +0530

    Time: 39 ms (15.83%), Space: 156 MB (22.25%) - LeetHub

---
+++ 3840-house-robber-v/3840-house-robber-v.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long find(int[] nums, int[] colors, int i, long[]dp) {
+        if(i<0) return 0;
+
+        if(dp[i]!=-1)return dp[i];
+        long inc, notinc =0;
+        if(i!=0&& colors[i]==colors[i-1]) {
+            inc = nums[i] + find(nums, colors, i-2,dp);
+        }else {
+            inc = nums[i] + find(nums,colors,i-1,dp);
+        }
+
+        notinc = find(nums,colors, i-1,dp);
+
+        return dp[i]= Math.max(inc,notinc);
+    }
+
+
+    public long rob(int[] nums, int[] colors) {
+        int n = nums.length;
+        long[]dp= new long[n];
+        Arrays.fill(dp,-1);
+        return find(nums, colors,n-1,dp);
+    }
+}
